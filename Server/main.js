0929bb27d07bb52a1ca7baf36d5211c566713ce2
David <r.d.jorgensen@gmail.com>
2021-08-01 17:30:05 -0700

    ssl cert server

--- Server/main.js
+++ Server/main.js
@@ -1,34 +1,20 @@
 const express = require('express');
-//const helmet = require('helmet');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 const API_PORT = 8080;
 //these are temp imports
-const {photoData} = require('./rawPhotoData');
-const {postData} = require('./rawPostData');
+//const {photoData} = require('./rawPhotoData');
+//const {postData} = require('./rawPostData');
 
 const app = express();
-//app.use(helmet());
-app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}...`))
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
 
-app.get('/api/posts', (req, res) => {
-  return res.send(postData);
-});
+sslServer.listen(API_PORT, () => console.log(`Secure server on port ${API_PORT}...`))
 
-app.post('/api/posts', (req, res) => {
-  return res.send();
-});
-
-app.get('/api/photos', (req, res) => {
-  return res.send(photoData);
-});
-
-app.post('/api/photos', (req, res) => {
-  return res.send();
-});
-
-app.get('/api/images', (req, res) => {
-  return res.send();
-});
-
-app.post('/api/images', (req, res) => {
-  return res.send();
+app.get('/', (req, res, next) => {
+  res.send('Hello from SSL server');
 });
