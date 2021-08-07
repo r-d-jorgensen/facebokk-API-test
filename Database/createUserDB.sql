58e9eed0b40f0df0f7e751b7f47c86642094f9f4
David <r.d.jorgensen@gmail.com>
2021-08-06 18:25:12 -0700

    beter and another ENUM, disciptive names

--- Database/createUserDB.sql
+++ Database/createUserDB.sql
@@ -1,4 +1,4 @@
--- version 0.2.0
+-- version 0.3.0
 drop database if exists `user_db`;
 create database `user_db`;
 use `user_db`;
@@ -16,19 +16,28 @@ create table users (
 )  engine=innodb auto_increment=1 default charset=utf8mb4 collate = utf8mb4_0900_ai_ci;
 
 create table origin_ENUM (
+		origin_ENUM_id int not null,
     origin_type varchar(50) not null,
-    primary key (origin_type)
+    primary key (origin_ENUM_id)
+)  engine=innodb auto_increment=1 default charset=utf8mb4 collate = utf8mb4_0900_ai_ci;
+
+create table structure_Type_ENUM (
+		structure_type_ENUM_id int not null,
+    structure_type varchar(50) not null,
+    primary key (structure_type_ENUM_id)
 )  engine=innodb auto_increment=1 default charset=utf8mb4 collate = utf8mb4_0900_ai_ci;
 
 create table syncs (
   sync_id int not null auto_increment,
   user_id int not null,
-  origin_type varchar(50) not null,
-  sync_date datetime not null,
-  was_completed boolean not null,
+	origin_ENUM_id int not null,
+  structure_type_ENUM_id int not null,
+  synced_at datetime not null,
+  deepest_checkpoint varchar(150),
   primary key (sync_id),
   foreign key (user_id) references users(user_id) on delete cascade,
-  foreign key (origin_type) references origin_ENUM(origin_type)
+  foreign key (origin_ENUM_id) references origin_ENUM(origin_ENUM_id),
+  foreign key (structure_type_ENUM_id) references structure_Type_ENUM(structure_type_ENUM_id)
 ) engine=innodb auto_increment=1 default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
 
 create table images (
@@ -45,24 +54,24 @@ create table posts (
   post_id int not null auto_increment,
   user_id int not null,
   image_id int not null,
-  origin_type varchar(50) not null,
-  created_date date not null,
+  origin_ENUM_id int not null,
+  created_at date not null,
   message varchar(500),
   primary key (post_id),
   foreign key (user_id) references users(user_id) on delete cascade,
   foreign key (image_id) references images(image_id ) on delete cascade,
-  foreign key (origin_type) references origin_ENUM(origin_type)
+  foreign key (origin_ENUM_id) references origin_ENUM(origin_ENUM_id)
 ) engine=innodb auto_increment=1 default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
 
 create table photos (
-  photos_id int not null auto_increment,
+  photo_id int not null auto_increment,
   user_id int not null,
   image_id int not null,
-  origin_type varchar(50) not null,
-  created_date date not null,
+  origin_ENUM_id int not null,
+  created_at date not null,
   caption varchar(500),
-  primary key (photos_id),
+  primary key (photo_id),
   foreign key (user_id) references users(user_id) on delete cascade,
   foreign key (image_id) references images(image_id) on delete cascade,
-  foreign key (origin_type) references origin_ENUM(origin_type)
+  foreign key (origin_ENUM_id) references origin_ENUM(origin_ENUM_id)
 ) engine=innodb auto_increment=1 default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
